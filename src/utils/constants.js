123f359fa096e376a50fb8ea6ea761c3aa87431b
Rupert <rupert.bothma1@gmail.com>
2021-07-01 14:01:30 +0200

    test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 import React from 'react'
+require('dontenv').config()
+const Airtable = require('airtable')
 import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
 export const links = [
   {
@@ -41,7 +43,36 @@ export const services = [
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
 ]
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+var base = Airtable.base('appDEHeH8EDQ13r7L');
 
-export const products_url = 'https://api.airtable.com/v0/appDEHeH8EDQ13r7L/Table%201?api_key=keyhVfHtuogPhOeoT.com/react-store-products'
+exports.handler = async (event,context, cb) => {
+    try {
+        const data = await airtable.list()
+        const products = records.map((product) => {
+            const {id} = product
+            const {name,price,image,colors,company,description,shipping} = product.fields
+            const url = image[0].url
+            return{id,name,price,url,colors,company,description,shipping}
+        })
+        return{
+            statusCode: 200,
+            body: JSON.stringify(products),
+            }
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`
+    } catch (error){
+        return{
+            statusCode: 500,
+            body: 'Server Error',
+            }
+    }
+   
+}
+export const products_url = 'https://api.airtable.com/v0/appDEHeH8EDQ13r7L/Table%201?api_key=keyhVfHtuogPhOeoT'
+
+export const single_product_url = 'https://api.airtable.com/v0/appDEHeH8EDQ13r7L/Table%201?api_key=keyhVfHtuogPhOeoT'
+
