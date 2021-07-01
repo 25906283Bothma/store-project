8291d1bd9d518f6fefeced4e5cd4f84c75442027
Rupert <rupert.bothma1@gmail.com>
2021-07-01 14:02:57 +0200

    try

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -41,29 +41,6 @@ export const services = [
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
 ]
-
-exports.handler = async (event,context, cb) => {
-    try {
-        const data = await airtable.list()
-        const products = records.map((product) => {
-            const {id} = product
-            const {name,price,image,colors,company,description,shipping} = product.fields
-            const url = image[0].url
-            return{id,name,price,url,colors,company,description,shipping}
-        })
-        return{
-            statusCode: 200,
-            body: JSON.stringify(products),
-            }
-
-    } catch (error){
-        return{
-            statusCode: 500,
-            body: 'Server Error',
-            }
-    }
-   
-}
 export const products_url = 'https://api.airtable.com/v0/appDEHeH8EDQ13r7L/Table%201?api_key=keyhVfHtuogPhOeoT'
 
 export const single_product_url = 'https://api.airtable.com/v0/appDEHeH8EDQ13r7L/Table%201?api_key=keyhVfHtuogPhOeoT'
