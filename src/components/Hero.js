32bb59f87db1f45142b5665cbd1f05f0369fcc67
Rupert <rupert.bothma1@gmail.com>
2021-07-01 09:25:52 +0200

    photo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => {
     <Wrapper className='section-center'>
       <article className='content'>
         <h1>
-          design your <br />
-          comfort zone
+          Get your <br />
+          Coffee Fix
         </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iure
