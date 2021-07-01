2cfcb8cba31cece100d947d0b67e4c971e71add7
Rupert <rupert.bothma1@gmail.com>
2021-07-01 11:19:06 +0200

    test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -42,6 +42,6 @@ export const services = [
   },
 ]
 
-export const products_url = 'https://course-api.com/react-store-products'
+export const products_url = 'https://course-apihttps://api.airtable.com/v0/appDEHeH8EDQ13r7L/Table%201?api_key=keyhVfHtuogPhOeoT.com/react-store-products'
 
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`
