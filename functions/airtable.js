29c5762b8ea0384f63fdc774de26adfe51121977
Rupert <rupert.bothma1@gmail.com>
2021-07-01 12:02:28 +0200

    airtable

---
+++ functions/airtable.js
@@ -0,0 +1,17 @@
+const Airtable = require('airtable')
+
+const airtable = new Airtable({apiKey: keyhVfHtuogPhOeoT})
+.base('appDEHeH8EDQ13r7L')
+.table('store')
+
+exports.handler = async (event,context, cb) => {
+    try {
+        const data = await airtable.list()
+        console.log(data)
+    } catch (error){
+
+    }
+    return
+    statusCode: 200,
+    body: 'Airtable Example',
+}
